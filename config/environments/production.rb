096d65f40b9c1c43368dcca3e14e1ab80abe8bbb
Arthur Bershadskiy <arthurbershadskiy@gmail.com>
2013-12-18 22:19:48 -0500

    changed root to store#index

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,6 +5,7 @@ Depot::Application.configure do
   config.cache_classes = true
 
   # Eager load code on boot. This eager loads most of Rails and
+
   # your application in memory, allowing both thread web servers
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
