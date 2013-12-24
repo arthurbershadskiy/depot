2a9d11ffdce54fc8c3e71cb5301cfa517dc59242
Arthur Bershadskiy <arthurbershadskiy@gmail.com>
2013-12-24 17:07:20 -0500

    added cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,15 @@ class Product < ActiveRecord::Base
 			:with => %r{.(gif|jpg|png|GIF|JPG|PNG|GiF)},
       :message => 'must be a url for gif, jpg, or png.'
 	}
+	has_many :line_items
+	before_destroy :ensure_not_referenced_by_any_line_item
+	private
+	def ensure_not_referenced_by_any_line_item
+		if line_items.empty?
+			return true
+		else
+			errors.add(:base, 'This product is in someone\'s cart')
+			return false
+		end
+	end
 end
